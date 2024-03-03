98106b3138f52ea1752239dfe7a2ae226320844c
sivakumar6678 <125382940+sivakumar6678@users.noreply.github.com>
2024-03-03 14:47:13 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -3,10 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "Results";
-// $servername = "sql301.ezyro.com";
-// $username = "ezyro_35935551";
-// $password = "1900b6b69d";
-// $dbname = "ezyro_35935551_Results";
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
